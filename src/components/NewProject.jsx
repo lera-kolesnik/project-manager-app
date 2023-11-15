b511e5868b43bd9b8fc848c7bb38c48088fb563a
Valeriia Kolesnyk <kolesnik.va8@gmail.com>
2023-11-15 14:12:52 +0000

    added logic to collect user inputs using Refs, currently without validation the data

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,20 +1,38 @@
+import { useRef } from "react";
 import Input from "./Input.jsx";
 
 export default function NewProject() {
+  const title = useRef();
+  const description = useRef();
+  const dueDate = useRef();
+
+  function handleSave() {
+    const enteredTitle = title.current.value;
+    const enteredDescription = description.current.value;
+    const enteredDueDate = dueDate.current.value;
+  }
+
   return (
     <div className="w-[35rem] mt-16">
       <menu className="flex item-center justify-end gap-4 my-4">
         <li>
-          <button className="py-2 text-stone-800 hover:text-stone-950">Cancel</button>
+          <button className="py-2 text-stone-800 hover:text-stone-950">
+            Cancel
+          </button>
         </li>
         <li>
-          <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button>
+          <button
+            className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+            onClick={handleSave}
+          >
+            Save
+          </button>
         </li>
       </menu>
       <div>
-        <Input label="Title" />
-        <Input label="Description" textarea />
-        <Input label="Due Date" />
+        <Input ref={title} label="Title" />
+        <Input ref={description} label="Description" textarea />
+        <Input ref={dueDate} label="Due Date" />
       </div>
     </div>
   );
