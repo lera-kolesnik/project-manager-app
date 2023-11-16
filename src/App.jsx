4d5cda59f7d50ae68c86a599679f055c0ab1b5c9
Valeriia Kolesnyk <kolesnik.va8@gmail.com>
2023-11-16 16:58:57 +0000

    updating the state to go back to the fallback screen, setting the selected project id

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { unmountComponentAtNode } from "react-dom";
 import { v4 as uuidv4 } from "uuid";
 import NewProject from "./components/NewProject.jsx";
 import NoProjectSelected from "./components/NoProjectSelected.jsx";
@@ -21,18 +22,18 @@ function App() {
 
   function handleAddProject(projectData) {
     setProjectsState((prevState) => {
+      const projectId = uuidv4();
       const newProject = {
         ...projectData,
-        id: uuidv4(),
+        id: projectId,
       };
       return {
         ...prevState,
+        selectedProjectId: undefined,
         projects: [...prevState.projects, newProject],
       };
     });
   }
-
-  console.log(projectsState);
 
   let content;
 
@@ -44,7 +45,10 @@ function App() {
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectsSidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectsState.projects}
+      />
       {content}
     </main>
   );
