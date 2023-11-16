4570250d1b23bec01fa8391e67f95e91a6d02fd4
Valeriia Kolesnyk <kolesnik.va8@gmail.com>
2023-11-16 17:41:52 +0000

    added a ‘Modal’ component for showing an error modal as well, however it could be reusable to show other kinds of info

---
+++ src/components/Modal.jsx
@@ -0,0 +1,21 @@
+import { useRef } from "react";
+import { forwardRef, useImperativeHandle, useRef } from "react";
+import { createPortal } from "react-dom";
+
+const Modal = forwardRef(function Modal({ children }, ref) {
+  const dialog = useRef();
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      },
+    };
+  });
+
+  return createPortal(
+    <dialog ref={dialog}>{children}</dialog>,
+    document.querySelector("#modal-root")
+  );
+});
+
+export default Modal;
