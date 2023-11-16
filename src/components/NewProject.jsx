bfc73ec76ecab905cc1b0d849e74042f17034ac7
Valeriia Kolesnyk <kolesnik.va8@gmail.com>
2023-11-16 18:19:34 +0000

    added missed caption and clear code

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -32,9 +32,9 @@ export default function NewProject({ onAdd }) {
 
   return (
     <>
-      <Modal ref={modal}>
+      <Modal ref={modal} buttonCaption="Okay">
         <h2>Invalid Input</h2>
-        <p>Oops... looks like you forgit to enter a value</p>
+        <p>Oops... looks like you forgot to enter a value</p>
         <p>Please make sure you provide a valid value for every input field</p>
       </Modal>
       <div className="w-[35rem] mt-16">
