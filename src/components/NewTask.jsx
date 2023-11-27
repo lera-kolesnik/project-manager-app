d083b3124530e4164877432525bb446903e6a6a7
Valeriia Kolesnyk <kolesnik.va8@gmail.com>
2023-11-27 20:21:58 +0000

    added enter functionality and updated styles

--- src/components/NewTask.jsx
+++ src/components/NewTask.jsx
@@ -7,6 +7,12 @@ export default function NewTask({ onAdd }) {
     setEnteredTask(event.target.value);
   }
 
+  function handleKeyDown(event) {
+    if (event.key === "Enter") {
+      handleClick();
+    }
+  }
+
   function handleClick() {
     if (enteredTask.trim() === "") {
       return;
@@ -19,12 +25,13 @@ export default function NewTask({ onAdd }) {
     <div className="flex items-center gap-4">
       <input
         type="text"
-        className="w-64 px-2 py-1 rounded-sm bg-stone-200"
+        className="w-64 px-2 py-1 rounded-sm bg-gray-200"
         onChange={handleChange}
+        onKeyDown={handleKeyDown}
         value={enteredTask}
       />
       <button
-        className="text-stone-700 hover:text-stone-950"
+        className="px-1 py-1 rounded-md text-lg text-gray-700 text-decoration-line: underline hover:text-gray-950 hover:bg-indigo-200"
         onClick={handleClick}
       >
         Add Task
