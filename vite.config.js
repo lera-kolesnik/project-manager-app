393b8c2f8ab966ccfb6a7b60205c7c2f3801a2b1
Valeriia Kolesnyk <kolesnik.va8@gmail.com>
2023-12-01 20:08:08 +0000

    addressing the issue with the vite builder to build the project and display it not only locally

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/dist/',
   plugins: [react()],
+  esbuild: {
+    loader: 'jsx',
+  },
+  resolve: {
+    alias: {
+      './runtimeConfig': './runtimeConfig.browser',
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
 })
