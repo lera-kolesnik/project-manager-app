890deb2884aaf87c7b783e4f87438b300c10f00a
Valeriia Kolesnyk <kolesnik.va8@gmail.com>
2023-12-02 20:44:22 +0000

    building setup

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/dist/',
+  base: 'https://lera-kolesnik.github.io/project-manager-app',
   plugins: [react()],
   esbuild: {
     loader: 'jsx',
